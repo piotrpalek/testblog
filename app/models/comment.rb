adf6a8331e146f8c5778c90e55e4176d0455a5ff
Piotr Palek <piotrpalek@gmail.com>
2014-01-22 21:33:06 +0100

    create basic comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,12 @@
 class Comment
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :body, type: String
+
+  validates_presence_of :body
+
+  belongs_to :user
+  belongs_to :post
+
 end
