a1b481aaac8285c684975bce1e8be3b51a898d15
Piotr Palek <piotrpalek@gmail.com>
2014-01-22 21:11:53 +0100

    permit additional name param for sign_up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
+  before_filter :update_sanitized_params, if: :devise_controller?
   protect_from_forgery
 
   decent_configuration do
     strategy DecentExposure::StrongParametersStrategy
   end
 
-
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
 end
