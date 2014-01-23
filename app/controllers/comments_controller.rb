c131df34551611bc3497933c05ae7847b503b875
Piotr Palek <piotrpalek@gmail.com>
2014-01-23 21:57:41 +0100

    create comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  before_filter :authenticate_user!
+  expose(:post)
+  expose(:comments) { post.comments }
+  expose(:comment)
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    if comment.save
+      redirect_to comment.post, :notice => "Successfully created comment."
+    else
+      render :new
+    end
+  end
+
 end
