a8bb3004b1b71aa2d638745575d61f0477578066
Piotr Palek <piotrpalek@gmail.com>
2014-01-24 10:05:11 +0100

    add hotness method to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,4 +19,19 @@ class Post
     update_attribute :archived, true
   end
 
+  def hotness
+    hotness = self.comments.count >= 3 ? 1 : 0
+
+    if self.created_at >= 1.days.ago
+      return hotness + 3
+    elsif self.created_at >= 3.days.ago
+      return hotness + 2
+    elsif self.created_at >= 7.days.ago
+      return hotness + 1
+    else self.created_at > 7.days.ago
+      return hotness
+    end
+
+  end
+
 end
