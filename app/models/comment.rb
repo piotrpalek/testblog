ef119316e4d6ccdd7608c973c66486628533f77d
Piotr Palek <piotrpalek@gmail.com>
2014-01-24 12:02:20 +0100

    create vote_up method in comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -19,4 +19,19 @@ class Comment
   def mark_not_abusive!
     update_attribute :abusive, false
   end
+
+  def vote_up(user_id)
+    vote = vote_for_user(user_id)
+    vote.value = 1
+    vote.save
+  end
+
+  private
+
+  def vote_for_user(user_id)
+    Vote.where(
+      user_id: user_id,
+      comment_id: id
+    ).first_or_create
+  end
 end
