93341cc04f2a8fe90320b8c2d4837c4f2d0f685c
Piotr Palek <piotrpalek@gmail.com>
2014-01-24 10:49:59 +0100

    create vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,11 @@
 class Vote
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :score, type: Integer
+
+  validates_presence_of :score
+
+  belongs_to :user
+  belongs_to :comment
 end
