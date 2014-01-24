d0460232a91d4f110afa1138c4fa6b373891ba92
Piotr Palek <piotrpalek@gmail.com>
2014-01-24 11:11:50 +0100

    add mark_as_not_abusive route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Easyblog::Application.routes.draw do
   devise_for :users
   resources :users
   resources :posts do
-  	resources :comments
+  	resources :comments do
+      member do
+        post :mark_as_not_abusive
+      end
+    end
     member do
       post :mark_archived
     end
